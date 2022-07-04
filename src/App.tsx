6ef221bebaf6a03bdbc335ba0ff7fac4ef0e457c
= <=>
2022-07-04 14:30:37 -0700

    build the file structure and add tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-3xl font-bold">
+      Abdurrahman || Ali ? "software engneering continue" : "software engneering will end"
+    </div>
+  );
+}
+
+export default App;
