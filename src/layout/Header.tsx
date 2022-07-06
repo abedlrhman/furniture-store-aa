40155911a747eeadc4f289cc365cf8bead7239d4
= <=>
2022-07-06 14:54:24 -0700

    fix responsive

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -17,24 +17,24 @@ function Header ( { }: Props )
     <div>
       <div className='container mx-auto flex justify-between h-16 item-center px-3'>
         <div className="flex flex-col gap-2 justify-center">
-          <h1 className="text-4xl leading-3	">furniture</h1>
-          <span className="text-sm">Modern & minimalistic</span>
+          <h1 className="text-3xl leading-3	">FURNITURE</h1>
+          <span className="text-sm text-center">Modern & minimalistic</span>
         </div>
         <div className="flex flex-row-reverse md:flex-row font-light gap-8 items-center">
-          <div className="md:hidden">
+          <div className="md:hidden group">
             <AiOutlineMenu />
 
-            <div className="absolute max-h-0 group-hover:max-h-96 md:max-h-full overflow-hidden transition-all w-full right-0 top-0">
+            <ul className="absolute max-h-0 group-hover:max-h-96 md:max-h-full overflow-hidden transition-all w-full right-0 top-0 bg-white ">
               {
                 HeaderData.data.map( headerItem =>
                 (
-                  <li key={ headerItem.id } className="text-zinc-700 hover:text-black"><Link to={ headerItem.url }>{ headerItem.name }</Link></li>
+                  <li key={ headerItem.id } className="text-zinc-700 hover:text-black border-b-2 border-b-gray-600 py-1 text-center"><Link to={ headerItem.url }>{ headerItem.name }</Link></li>
 
                 ) )
               }
 
 
-            </div>
+            </ul>
           </div>
           <ul className="hidden md:flex gap-8 text-lg ">
 
