4f5b2aa4c01264e3714d07489a4dbf3c1b79d823
= <=>
2022-07-06 13:59:55 -0700

    add header as static for now

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import { AiOutlineUser, AiOutlineSearch, AiOutlineHeart } from "react-icons/ai"
+import { BsHandbag } from "react-icons/bs"
 
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <div>Header</div>
+    <div>
+      <div className='container mx-auto bg-red-400 flex justify-between h-16 item-center px-3'>
+        <div className="flex flex-col gap-2 justify-center">
+          <h1 className="text-4xl leading-3	">furniture</h1>
+          <span className="text-sm">Modern & minimalistic</span>
+        </div>
+        <div className="flex font-light gap-8 items-center">
+          <ul className="flex gap-8 text-lg ">
+            <li className="text-zinc-700 hover:text-black"><a href="#">Home</a></li>
+            <li>Catalog</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
+          <ul className="flex gap-3 text-2xl">
+            <li>
+              <AiOutlineSearch />
+            </li>
+            <li>
+              <BsHandbag />
+            </li>
+            <li>
+              <AiOutlineHeart />
+            </li>
+            <li>
+              <AiOutlineUser />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
