53f51979210caeeb3d41522701f45af855f86c6d
= <=>
2022-07-07 14:11:36 -0700

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      customgray: "#7b8799",
+      custombuedgray: "#465773",
+      customnight: "#11284d",
+      customlight: "#f7faff"
+    }
   },
   plugins: [],
 };
